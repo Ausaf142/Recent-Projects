a814bbb49bdb2225821668468d676bcd99b4f2bd
Ausaf142 <ausaf142@gmail.com>
2024-02-16 00:40:16 +0530

    WrongXpath

--- jenkins/src/test/java/jenkinsTest/prog2.java
+++ jenkins/src/test/java/jenkinsTest/prog2.java
@@ -1,7 +1,15 @@
 package jenkinsTest;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class prog2 {
 public static void main(String[] args) {
 	System.out.println("sad");
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.google.com");
+	driver.findElement(By.id("APjFqb")).sendKeys("www.amarkumar.art.blog");
+	driver.findElement(By.name("btnK")).click();
 }
 }
